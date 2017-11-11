82a8c3a6ccf36cbb51a23b04f81d4f9e64d6005c
502581120 <furog.jerome@ge.com>
2017-11-11 17:20:32 +0800

    google maps only

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NavController, Platform } from 'ionic-angular';
+
+declare var google: any;
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  @ViewChild('map') mapRef: ElementRef;
+  map: any;
 
+  constructor(public navCtrl: NavController, public platform: Platform) {
+    platform.ready().then(() => {
+      //this.showMap();
+    });
   }
 
+  ionViewDidLoad() {
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(14.555830, 121.023909);
+
+    const options = {
+      center: location,
+      zoom: 16
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 }
