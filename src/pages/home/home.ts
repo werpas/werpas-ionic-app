643544bcd6b127c193e562f7826181cea63b0ca7
502581120 <furog.jerome@ge.com>
2017-11-12 01:08:22 +0800

    google maps only

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController, Platform } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 declare var google: any;
 
@@ -9,27 +10,62 @@ declare var google: any;
 })
 export class HomePage {
 
-  @ViewChild('map') mapRef: ElementRef;
+  @ViewChild('map') mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController, public platform: Platform) {
-    platform.ready().then(() => {
-      //this.showMap();
-    });
+  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+    
   }
 
   ionViewDidLoad() {
     this.showMap();
   }
 
-  showMap() {
-    const location = new google.maps.LatLng(14.555830, 121.023909);
+  showMap() {    
 
-    const options = {
-      center: location,
-      zoom: 16
-    };
+    this.geolocation.getCurrentPosition().then((position) => {
+      
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+      let mapOptions = {
+        center: latLng,
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
+      }
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      this.addMarker();
+
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  addMarker(){
+    
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+
+    let content = "<h4>Information!</h4>";         
+
+    this.addInfoWindow(marker, content);
+
+  }
+
+  addInfoWindow(marker, content){
+    
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+  
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  
   }
 }
